d1c4e82905fb93305c969338039cc29d3eabff52
Mona Lisa <sofiasamuels@gmail.com>
2025-06-25 11:32:07 -0400

    changes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,11 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
-const {logger} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/v2/https");
-const {onDocumentCreated} = require("firebase-functions/v2/firestore");
+import * as logger from "firebase-functions"
+import {onRequest} from "firebase-functions/v2/https";
+import {onDocumentCreated} from "firebase-functions/v2/firestore";
 
 // The Firebase Admin SDK to access Firestore.
-const {initializeApp} = require("firebase-admin/app");
-const {getFirestore} = require("firebase-admin/firestore");
+import {initializeApp} from "firebase-admin/app";
+import {getFirestore} from "firebase-admin/firestore";
 
 initializeApp();
 
