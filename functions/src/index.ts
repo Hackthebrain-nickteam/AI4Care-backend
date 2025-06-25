9d1afc746359bae01932759ffb6c6d049e128f82
Mona Lisa <sofiasamuels@gmail.com>
2025-06-25 11:37:25 -0400

    fix lint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
-import * as logger from "firebase-functions"
+import {logger} from "firebase-functions";
 import {onRequest} from "firebase-functions/v2/https";
 import {onDocumentCreated} from "firebase-functions/v2/firestore";
 
@@ -16,8 +16,8 @@ exports.addmessage = onRequest(async (req:any, res:any) => {
   const original = req.query.text;
   // Push the new message into Firestore using the Firebase Admin SDK.
   const writeResult = await getFirestore()
-      .collection("messages")
-      .add({original: original});
+    .collection("messages")
+    .add({original: original});
   // Send back a message that we've successfully written the message
   res.json({result: `Message with ID: ${writeResult.id} added.`});
 });
@@ -25,18 +25,19 @@ exports.addmessage = onRequest(async (req:any, res:any) => {
 // Listens for new messages added to /messages/:documentId/original
 // and saves an uppercased version of the message
 // to /messages/:documentId/uppercase
-exports.makeuppercase = onDocumentCreated("/messages/{documentId}", (event:any) => {
+exports.makeuppercase = onDocumentCreated("/messages/{documentId}",
+  (event:any) => {
   // Grab the current value of what was written to Firestore.
-  const original = event.data.data().original;
+    const original = event.data.data().original;
 
-  // Access the parameter `{documentId}` with `event.params`
-  logger.log("Uppercasing", event.params.documentId, original);
+    // Access the parameter `{documentId}` with `event.params`
+    logger.log("Uppercasing", event.params.documentId, original);
 
-  const uppercase = original.toUpperCase();
+    const uppercase = original.toUpperCase();
 
-  // You must return a Promise when performing
-  // asynchronous tasks inside a function
-  // such as writing to Firestore.
-  // Setting an 'uppercase' field in Firestore document returns a Promise.
-  return event.data.ref.set({uppercase}, {merge: true});
-});
+    // You must return a Promise when performing
+    // asynchronous tasks inside a function
+    // such as writing to Firestore.
+    // Setting an 'uppercase' field in Firestore document returns a Promise.
+    return event.data.ref.set({uppercase}, {merge: true});
+  });
